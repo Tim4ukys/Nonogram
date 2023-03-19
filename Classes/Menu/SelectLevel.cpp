86fdb7b039185411084c24988f0d7ab416fc7624
Tim4ukys <tim4ukys.dev@yandex.ru>
2023-03-20 01:31:05 +0700

    add scrollview

--- Classes/Menu/SelectLevel.cpp
+++ Classes/Menu/SelectLevel.cpp
@@ -3,6 +3,8 @@
 #include "DefColors.hpp"
 #include "json.hpp"
 
+#include "ui/CocosGUI.h"
+
 USING_NS_CC;
 
 bool SelectLevel::init()
@@ -24,6 +26,32 @@ bool SelectLevel::init()
 
     const auto visibleSize = Director::getInstance()->getVisibleSize();
     const Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto scrl = ui::ScrollView::create();
+    scrl->setDirection(ui::ScrollView::Direction::VERTICAL);
+    // scrl->setBackGroundColor({0xac, 0xac, 0xac});
+    scrl->setBackGroundColorType(ui::Layout::BackGroundColorType::NONE);
+    // scrl->setScrollBarAutoHideEnabled(true);
+    scrl->setScrollBarEnabled(false);
+
+    const float srclWidth = visibleSize.width - 20*2;
+    const float maxH = visibleSize.height - 50 /*низ*/ - 15 /*верх*/;
+    
+    float contentSize{};
+    for (int i{}; i < 50; ++i) {
+        auto btn = Label::createWithTTF("ZZZZZZ VVVVVV", "fonts/vga8x16.ttf", 16.f);
+        const float h = 15 + btn->getContentSize().height;
+        contentSize += h;
+        btn->setPosition({srclWidth/2.f, i*h + 15});
+        scrl->addChild(btn);
+    }
+    
+    scrl->setContentSize({srclWidth, MIN(maxH, contentSize)});
+    scrl->setInnerContainerSize({srclWidth, contentSize});
+    scrl->setAnchorPoint({0.0f, 1.0f});
+    scrl->setPosition({origin.x + 20, origin.y + visibleSize.height - 15});
+    scrl->setBounceEnabled(contentSize > maxH);
+    this->addChild(scrl);
 
     auto back = MenuItemLabel::create(Label::createWithTTF(FontManager::mainMenu, "Назад"), [](Ref* sender){ Director::getInstance()->popScene(); });
 
--- Classes/Menu/SelectLevel.h
+++ Classes/Menu/SelectLevel.h
@@ -4,14 +4,6 @@
 #include "cocos2d.h"
 
 class SelectLevel : public cocos2d::Scene {
-
-    class SelectLayer : public cocos2d::Layer {
-    public:
-        static inline cocos2d::Layer* createLayer() { return SelectLayer::create(); }
-
-        CREATE_FUNC(SelectLayer);
-    };
-
 public:
     static inline cocos2d::Scene* createScene() { return SelectLevel::create(); }
 
