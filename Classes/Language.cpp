2553ff47b740409c72ab7047b926cc5c7b6206ff
Tim4ukys <tim4ukys.dev@yandex.ru>
2023-03-25 20:15:03 +0700

    add fullName language

--- Classes/Language.cpp
+++ Classes/Language.cpp
@@ -2,19 +2,31 @@
 // Created by tim4ukys on 25.03.2023.
 //
 
+#include <filesystem>
+#include <cocos2d.h>
 #include "Language.hpp"
 #include "json.hpp"
 #include "Snippets.hpp"
 
-Language::Language(Config& config) {
-    nlohmann::json j;
-    snippets::loadJSON("conf/lang/" + config["language"].get<std::string>() + ".json", j);
+USING_NS_CC;
 
-    for (auto& [key, data] : j.items()) {
-        menuItem item;
-        for (auto& [k, l] : data.items()) {
-            item.insert({k, l.get<std::string>()});
+Language::Language(Config& config) {
+    for (auto& files : std::filesystem::directory_iterator(FileUtils::getInstance()->getDefaultResourceRootPath() + "conf/lang/")) {
+        const auto lKey = snippets::getFileName(files.path().string());
+        auto fd = FileUtils::getInstance()->getDataFromFile(files.path().string());
+        nlohmann::json j = nlohmann::json::parse(std::string((char*)fd.getBytes(), (size_t)fd.getSize()));
+        auto fullName = j["name"].get<std::string>();
+        j.erase("name");
+
+        dataItem dataItem1;
+        for (auto& [key, data] : j.items()) {
+            menuItem item;
+            for (auto& [k, l] : data.items()) {
+                item.insert({k, l.get<std::string>()});
+            }
+            dataItem1.insert({key, item});
         }
-        m_lang.insert({key, item});
+        m_lang.insert({lKey, {dataItem1, fullName}});
     }
+    m_curData = &m_lang[config["language"].get<std::string>()].first;
 }
--- Classes/Language.hpp
+++ Classes/Language.hpp
@@ -5,21 +5,26 @@
 #ifndef NONOGRAM_LANGUAGE_HPP
 #define NONOGRAM_LANGUAGE_HPP
 
+#include <map>
+#include <utility>
 #include <string_view>
+#include <memory>
 #include "Config.hpp"
 
 class Language {
-    static inline Language* s_pLang;
-    using menuItem = std::map<std::string, std::string>;
-    std::map<std::string, menuItem> m_lang;
+    static inline std::unique_ptr<Language> s_pLang;
+    using menuItem = std::map<std::string, std::string>; // переводы слов в меню по ключу
+    using dataItem = std::map<std::string, menuItem>; // структура со всеми "менюшками"
+    dataItem* m_curData = nullptr;
+    std::map<std::string, std::pair<dataItem/* data */, std::string /* full name */>> m_lang; // все языки
 
 public:
     explicit Language(Config& config);
 
-    static inline void init(Config& config){ s_pLang = new Language(config); };
+    static inline void init(Config& config){ s_pLang = std::make_unique<Language>(config); };
     static inline auto& getInstance() { return *s_pLang; };
 
-    inline auto& operator[](const std::string_view& key) { return m_lang[key.data()]; }
+    inline auto& operator[](const std::string_view& key) { return (*m_curData)[key.data()]; }
 };
 
 
